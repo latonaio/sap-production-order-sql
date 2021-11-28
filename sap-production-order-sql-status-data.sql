cacb9babf9d33f922faa285a0ebf515ae21f602d
Yurino Oga <yurino.o@latona.io>
2021-11-28 15:34:54 +0900

    release

--- sap-production-order-sql-status-data.sql
+++ sap-production-order-sql-status-data.sql
@@ -2,7 +2,7 @@ CREATE TABLE `sap-production-order-status-data`
 (
     `ManufacturingOrder`    varchar(12) DEFAULT NULL,
     `StatusCode`            varchar(40) DEFAULT NULL,
-    `IsUserStatus`          varchar(1) DEFAULT NULL,
+    `IsUserStatus`          tinyint(1) DEFAULT NULL,
     `StatusShortName`       varchar(40) DEFAULT NULL,
     `StatusName`            varchar(40) DEFAULT NULL,
     PRIMARY KEY (`ManufacturingOrder`),
